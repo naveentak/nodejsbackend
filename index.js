c6b990b7e85b69695aaeb3d70a919064c354024f
Naveen Tak <naveentak@gmail.com>
2018-07-11 05:31:11 +0200

    1. NodeJS finished 2. Express Server done 3. postman collection 4. routes, models and controller

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes';
+import mongo from 'mongoose';
+import bodyParser from 'body-parser';
 
 const appserver = express();
 const PORT = 9090;
+
+
+// mongoose connection
+mongo.Promise = global.Promise;
+mongo.connect('mongodb://mongo:27017/customerdb',{ useNewUrlParser: true });
+
+
+//bodyparser setup
+appserver.use(bodyParser.urlencoded({extended: true}));
+appserver.use(bodyParser.json());
+
+routes(appserver);
 
 appserver.get('/', (req, res) =>{
     res.send(`Node and express server is running on port ${PORT}`)
